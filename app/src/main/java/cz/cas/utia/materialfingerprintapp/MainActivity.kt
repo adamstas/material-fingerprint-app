04794ec9de6b87578d4f74afdc150c815bdc19c5
Adam Staš <stasadam@fit.cvut.cz>
2024-08-10 17:08:14 +0200

    add main navigation bar (just the UI)

--- app/src/main/java/cz/cas/utia/materialfingerprintapp/MainActivity.kt
+++ app/src/main/java/cz/cas/utia/materialfingerprintapp/MainActivity.kt
@@ -6,28 +6,98 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import cz.cas.utia.materialfingerprintapp.ui.theme.MaterialFingerprintAppTheme
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() //todo nechat? je to tu automaticky
         setContent {
             MaterialFingerprintAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val items = listOf( //todo move these variables to somewhere else?
+                    BottomNavigationItem(
+                        title = "Camera",
+                        selectedIcon = Icons.Filled.Home, //todo later change for camera
+                        unselectedIcon = Icons.Outlined.Home
+                    ),
+                    BottomNavigationItem(
+                        title = "Analytics",
+                        selectedIcon = Icons.Filled.ShoppingCart, //todo later change for analytics
+                        unselectedIcon = Icons.Outlined.ShoppingCart
+                    ),
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings
+                    ),
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
                 }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            //navController.navigate(item.title)
+                                        },
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (selectedItemIndex == index) item.selectedIcon else item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
+
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    )
+                    {} //todo scaffold content?
+                }
+
             }
         }
     }
+
 }
 
 @Composable
