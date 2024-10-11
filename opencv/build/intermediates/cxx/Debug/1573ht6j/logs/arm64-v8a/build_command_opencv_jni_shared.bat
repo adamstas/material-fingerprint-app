794824e87307ebb7160913c702183221de837239
Adam Staš <stasadam@fit.cvut.cz>
2024-10-11 11:36:43 +0200

    update .gitignore

--- opencv/build/intermediates/cxx/Debug/1573ht6j/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Android\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\AndroidStudioProjects\\MaterialFingerprintApp\\opencv\\.cxx\\Debug\\1573ht6j\\arm64-v8a" ^
-  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/1573ht6j/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Android\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\AndroidStudioProjects\\MaterialFingerprintApp\\opencv\\.cxx\\Debug\\1573ht6j\\x86" ^
-  opencv_jni_shared
